8006de0bb6d9254150ebba5b6201dc1b9b65bd2b
Luis Eduardo <juniorlluiseduardo@gmail.com>
2026-06-03 16:54:48 -0300

    final

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-import curses
-from selectors import SelectSelector
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('formulario')
-def exibir_formulario():
-    return render_template("formulario.html",resultado="Aguandando o envio...")
+@app.route('/')
+def formulario():
+    return render_template('formulario.html')
+
 
 @app.route('/processar', methods=['POST'])
 def processar_formulario():
@@ -16,21 +14,29 @@ def processar_formulario():
     idade = int(request.form['idade'])
     curso = request.form['curso']
 
-    if not nome or not idade or not curso:
-        mensagem_resultado = "Erro: Todos os campos são obrigatórios!"
-    else:
-        idade_int = int(idade)
-        mensagem_base = f"Olá{nome},você tem {idade} anos e está no curso de [{curso}]!"
-    if idade_int < 18:
-        mensagem_resultado = "Você é menor de idade"
-    elif idade_int > 18 and idade_int < 60:
+    mensagem_base = f"Olá {nome}, você tem {idade} anos e está no curso {curso}."
+
+    if idade < 18:
+        mensagem_idade = "Você é menor de idade."
+    elif idade < 60:
         mensagem_idade = "Você é adulto."
     else:
         mensagem_idade = "Você é experiente."
 
     if curso == "Python":
-        mensagem_curso = "Ótima escolha,você é versátil!"
+        mensagem_curso = "Ótima escolha, você é versátil!"
     elif curso == "Flask":
-        mensagem_curso = "Excelente escolha gafanhoto!"
-    elif curso == "HTML/CSS":
-        mensagem_curso = "Fundamental pequenino gafanhoto!"
+        mensagem_curso = "Excelente escolha, gafanhoto!"
+    else:
+        mensagem_curso = "Fundamental, pequenino gafanhoto!"
+
+    resultado = f"{mensagem_base} {mensagem_idade} {mensagem_curso}"
+
+    return render_template(
+        'formulario.html',
+        resultado=resultado
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
